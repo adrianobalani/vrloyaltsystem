dccc8b670a12954ceebec2d93b0e6e08079242a0
Adriano Rodrigues Balani <66102025+adrianobalani@users.noreply.github.com>
2024-05-24 00:45:46 -0300

    Update settings.py

--- vendas/settings.py
+++ vendas/settings.py
@@ -10,11 +10,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('KGL"_vr69*4-xMH`QA7HBm0x-Cs<j}G£x_/uG.>5*Ssh-l£FK8')
+SECRET_KEY = 'KGL"_vr69*4-xMH`QA7HBm0x-Cs<j}G£x_/uG.>5*Ssh-l£FK8'
 
 DEBUG = false
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
